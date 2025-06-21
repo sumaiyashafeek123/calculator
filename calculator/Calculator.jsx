0c463fbf6ff64d653632249dd942cd9c2b812d53
sumaiyashafeek123 <sumaiyashafeek123123@gmail.com>
2025-06-21 23:28:13 +0530

    my latest work

--- calculator/Calculator.jsx
+++ calculator/Calculator.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 function Calculator() {
-  return <div className="text-2xl text-red-400">Calculator</div>;
+  return (
+    <div className="text-2xl text-red-400">this is my simple Calculator</div>
+  );
 }
 
 export default Calculator;
